a7faf2e78d3dc50e732f46022e90e462da3b0751
liuxinglei1019 <921629101@qq.com>
2017-04-11 16:25:47 +0800

    ceshi

--- test/ViewController.m
+++ test/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"woshiceshi");
+    // Do any additional setup ；after loading the view, typically from a nib.
 }
 
 
